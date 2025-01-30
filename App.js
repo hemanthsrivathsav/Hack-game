8882ef25b3b5ba4cbc6ec07d3d20793cc2c673aa
hemanthsrivathsav <92975269+hemanthsrivathsav@users.noreply.github.com>
2025-01-30 15:53:25 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,23 +3,25 @@ import Grid from './Grid.js';
 
 // Usage example
 const App = () => {
-  const savedComponents = [
+ const savedComponents = [
     {
-      component_id: 1,
+      component_id: "550e8400-e29b-41d4-a716-446655440000",
       component_name: "WWW Endpoint",
       level: "1",
       row: 13,
       column: 8,
-      connected_to: [2],
+      points:0,
+      connected_to: ["550e8400-e29b-41d4-a716-446655440001"],
       src: "./node.png"
     },
     {
-      component_id: 2,
+      component_id: "550e8400-e29b-41d4-a716-446655440001",
       component_name: "Firewall",
       level: "1",
       row: 14,
       column: 10,
-      connected_to: [1],
+      points:10,
+      connected_to: ["550e8400-e29b-41d4-a716-446655440000"],
       src: "./firewall.png"
     }
   ];
